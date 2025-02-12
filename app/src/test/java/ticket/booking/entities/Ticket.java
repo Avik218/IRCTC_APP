ece1f806e157b8501891720e5d5210b8547ba337
tuhin chatterjee <tuhinchatterjee26@gmail.com>
2025-02-12 13:41:52 +0530

    A IRCTC APPLICATION With full Backend Service

---
+++ app/src/test/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,12 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String Source;
+    private String Destination;
+    private Date dateOfTravel;
+    private Train train;
+}
